868c397d1f0bff1a56776d978105ed7b84b3c66d
Thet Htet Aung <thet@tractive.com.my>
2021-09-19 13:09:31 +0630

    uncomment codes

--- src/components/Sections/Services/index.js
+++ src/components/Sections/Services/index.js
@@ -12,12 +12,11 @@ import Icon1 from '../../../images/svg-1.svg';
 import Icon2 from '../../../images/svg-2.svg';
 import Icon3 from '../../../images/svg-3.svg';
 const Services = () => {
-    const siteKey = '6LeGtpUbAAAAABg_QardUJiEBzFT5a86-72BMkAh';
     return (
         <>
             <ServicesContainer id='services'>
                 <ServicesH1>Our Services</ServicesH1>
-                {/* <ServicesWrapper>
+                <ServicesWrapper>
                     <ServicesCard>
                         <ServicesIcon src={Icon1} />
                         <ServicesH2>Reduce Expenses</ServicesH2>
@@ -33,13 +32,7 @@ const Services = () => {
                         <ServicesH2>Premium Benefits</ServicesH2>
                         <ServicesP>Unlock our special membership card that returns 5% cash back.</ServicesP>
                     </ServicesCard>
-                </ServicesWrapper> */}
-                <form action="/nodeform">
-                    <input type="text"/>
-                    <input type="text"/>
-                    <div class="g-recaptcha" data-sitekey={siteKey}></div>
-                    <input type="submit" />
-                </form>
+                </ServicesWrapper>
             </ServicesContainer>
         </>
     )
